9cc263a93d35b1cae4cd1f248b721c51ff8f8463
leicherbua@gmail.com <leicherbua@gmail.com@384f61c5-120f-5274-0ec6-c80045e338da>
2010-11-28 21:01:57 +0000

    LWTRT ServiceImpl Metrode zum Regestrieren der Port number erstellt "public void register"

--- trunk/Studienarbeit WS 2010 2011/src/main/java/edu/hm/dako/lwtrt/impl/LWTRTServiceImpl.java
+++ trunk/Studienarbeit WS 2010 2011/src/main/java/edu/hm/dako/lwtrt/impl/LWTRTServiceImpl.java
@@ -1,8 +1,13 @@
 package edu.hm.dako.lwtrt.impl;
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 import edu.hm.dako.lwtrt.LWTRTConnection;
 import edu.hm.dako.lwtrt.LWTRTService;
 import edu.hm.dako.lwtrt.ex.LWTRTException;
+import edu.hm.dako.udp.wrapper.UdpSocketWrapper;
 
 /**
  * The Implementation of LWTRTService.
@@ -12,9 +17,42 @@ import edu.hm.dako.lwtrt.ex.LWTRTException;
  */
 public class LWTRTServiceImpl implements LWTRTService {
 
-	@Override
+	// variable for logging
+	private static Log log = LogFactory.getLog(LWTRTService.class);
+
+	// variable for socketmap
+	static ConcurrentHashMap<Integer, UdpSocketWrapper> socketmap = new ConcurrentHashMap<Integer, UdpSocketWrapper>();
+
+	// variable for portnummer
+	int port;
+
+	/**
+	 * Registrieren einer Anwendung und Listenport aktivieren
+	 * <p>
+	 * Nach der Registrierung ist der lokale Port für den Dienstnehmer gebunden.
+	 * Ab dem Zeitpunkt der Registrierung werden eingehende Verbindungswünsche
+	 * entgegen genommen. Akzeptiert der Dienstnehmer die Verbindungswünsche
+	 * nicht rechtzeitig, wird der Verbindungsaufbau abgebrochen. Nach der
+	 * Registrierung kann der Dienstnehmer beginnen aktiv eine Verbindung
+	 * aufzubauen.
+	 * <p>
+	 * Ein Port kann nur einmal (auch durch andere Prozesse) registriert werden.
+	 * 
+	 * @param localPort
+	 *            Pornumber
+	 * @throws LWTRTException
+	 *             Fehler falls Portregestrierung nicht erflogleich war.
+	 * 
+	 */
 	public void register(int localPort) throws LWTRTException {
-		// TODO Auto-generated method stub
+		try {
+			UdpSocketWrapper udpSocketW = new UdpSocketWrapper(localPort);
+			socketmap.put(localPort, udpSocketW);
+			log.debug("INFO: Register LWTRTPort. Portnumber:"
+					+ LWTRTServiceImpl.socketmap.get(port));
+		} catch (Exception ex) {
+			log.error("ERROR: Fehler bei Regestrierung der Ports:" + ex);
+		}
 
 	}
 
