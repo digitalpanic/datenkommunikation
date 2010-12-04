9d479a5956b38987482ead076b1f988076b16886
m_hoang1985@web.de <m_hoang1985@web.de@384f61c5-120f-5274-0ec6-c80045e338da>
2010-12-04 15:48:17 +0000

    Chatsessionschicht läuft

--- trunk/Studienarbeit WS 2010 2011/src/main/java/edu/hm/dako/chatsession/impl/BaseServiceImpl.java
+++ trunk/Studienarbeit WS 2010 2011/src/main/java/edu/hm/dako/chatsession/impl/BaseServiceImpl.java
@@ -9,6 +9,9 @@ public abstract class BaseServiceImpl implements BaseSessionService {
 	private static Log log = LogFactory.getLog(BaseServiceImpl.class);
 	protected SessionStatus currentStatus = SessionStatus.NO_SESSION;
 
+	protected int listeport; 
+	protected String username;
+	
 	public enum SessionStatus {
 		NO_SESSION(1), SESSION_ACTIVE(2), SESSION_DESTROYED(3);
 		int code;
@@ -16,6 +19,14 @@ public abstract class BaseServiceImpl implements BaseSessionService {
 		SessionStatus(int code) {
 			this.code = code;
 		}
+	}
+	
+	public void setListenport(int listenport){
+		this.listeport=listenport;
+	}
+	
+	public String getUsername(){
+		return username;
 	}
 
 }
