d72c3f7b72f9dcd7088c15b9aa0e7e92c131d418
leicherbua@gmail.com <leicherbua@gmail.com@384f61c5-120f-5274-0ec6-c80045e338da>
2010-12-18 16:33:39 +0000

    Chatsession vom Pavlo comittet

--- Studienarbeit WS 2010 2011/src/main/java/edu/hm/dako/chatsession/impl/BaseServiceImpl.java
+++ Studienarbeit WS 2010 2011/src/main/java/edu/hm/dako/chatsession/impl/BaseServiceImpl.java
@@ -4,14 +4,19 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
 import edu.hm.dako.chatsession.BaseSessionService;
+import edu.hm.dako.chatsession.ex.ChatServiceException;
+import edu.hm.dako.chatsession.pdu.ChatPdu;
+import edu.hm.dako.lwtrt.LWTRTConnection;
+import edu.hm.dako.lwtrt.ex.LWTRTException;
 
 public abstract class BaseServiceImpl implements BaseSessionService {
 	private static Log log = LogFactory.getLog(BaseServiceImpl.class);
 	protected SessionStatus currentStatus = SessionStatus.NO_SESSION;
 
-	protected int listeport; 
+	protected int listeport;
 	protected String username;
-	
+	protected LWTRTConnection connection;
+
 	public enum SessionStatus {
 		NO_SESSION(1), SESSION_ACTIVE(2), SESSION_DESTROYED(3);
 		int code;
@@ -20,13 +25,39 @@ public abstract class BaseServiceImpl implements BaseSessionService {
 			this.code = code;
 		}
 	}
-	
-	public void setListenport(int listenport){
-		this.listeport=listenport;
+
+	/**
+	 * @param connection
+	 * @autor Pavlo Bishko
+	 */
+	public void setConnection(LWTRTConnection connection) {
+		this.connection = connection;
 	}
-	
-	public String getUsername(){
+
+	/**
+	 * @param cpdu
+	 * @throws ChatServiceException
+	 * @autor Pavlo Bishko
+	 */
+	protected void send(ChatPdu cpdu) throws ChatServiceException {
+		try {
+			connection.send(cpdu);
+		} catch (LWTRTException e) {
+			throw new ChatServiceException(e);
+		}
+
+	}
+
+	public void setListenport(int listenport) {
+		this.listeport = listenport;
+	}
+
+	public String getUsername() {
 		return username;
+	}
+
+	public void setUserName(String userName) {
+		this.username = userName;
 	}
 
 }
